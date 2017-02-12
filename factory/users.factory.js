33c857c0a55db22040ce2eabb5d2d5f27301f2e1
Mika Lindell <mika.lindell82@gmail.com>
2017-02-12 14:53:02 +0200

    Factory now creates files with different sortings to emulate API

--- factory/users.factory.js
+++ factory/users.factory.js
@@ -2,36 +2,73 @@ const faker = require('faker');
 const jsonfile = require('jsonfile')
 
 const total = 100;
-const file = './public/json/users.json'
+
+const file = {
+    prefix: './public/json/users_',
+    suffix: '.json'
+}
+
+const filters = [
+  'id',
+  'name',
+  'age',
+  'gender'
+]
+
+const save2Json = function(path, contents){
+  jsonfile.writeFile(path, {data: contents}, (err) => {
+    if(err) console.error(err);
+    else console.log("Saved to", path);
+  })
+}
 
 const iterator = new Array(total).fill(); // How many users to generate?
 let users = {
   data: null
 }
 
-console.log("Here's", total, "users 4 U.");
-
-users.data = iterator.map((_, i) => {
+users = iterator.map((_, id) => {
 
   const gender = faker.random.number(1);
 
-  console.log(faker.name.firstName);
-
   let user = {
-    id:     i,
-    name:   faker.name.lastName(1) + ', ' + faker.name.firstName(1),
+    id:     id,
+    name:   faker.name.firstName(gender) + ' ' + faker.name.lastName(gender),
     age:    faker.random.number(80),
     gender: gender === 0 ? 'male' : 'female'
   };
 
-  console.log(user);
-
   return user;
 });
 
-users.data.reverse()
+filters.forEach((sorting, i) => {
 
-jsonfile.writeFile(file, users, (err) => {
-  if(err) console.error(err);
-  else console.log("Saved to", file);
-})
+  let sorted = Array.from(users);
+
+  console.log(sorting);
+
+  sorted.sort((a, b) => {
+
+    if(typeof a[sorting] === 'string'){
+      const valA = a[sorting].toUpperCase();
+      const valB = b[sorting].toUpperCase();
+      if (valA < valB) return -1;
+      if (valA > valB) return 1;
+      return 0
+    }
+    else if(typeof a[sorting] === 'number'){
+      return a[sorting] - b[sorting];
+    }
+    else{
+      return 0;
+    }
+  });
+
+  console.log(sorted);
+
+  save2Json(`${file.prefix}${sorting}_asc${file.suffix}`, sorted);
+  sorted.reverse();
+  save2Json(`${file.prefix}${sorting}_desc${file.suffix}`, sorted);
+
+
+});
